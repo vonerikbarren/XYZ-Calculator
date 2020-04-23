d55a4dfa6955958cc6297a5ad8d6951349f6303c
Von'Erik Barren-Tucker <vonerikbarren-tucker@VonEriks-MacBook-Pro.local>
2020-04-22 20:38:27 -0400

    imports from material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//-------Material-UI-------------
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
 function App() {
   return (
     <div className="App">
+      <div>Fish</div>
       <h1 className="text-warning">Hello React</h1>
+
+
     </div>
   );
 }
