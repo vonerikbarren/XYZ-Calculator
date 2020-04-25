5be9c7b67e680b2b5edb913b9cd14d48f54af2a4
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-04-24 22:51:50 -0400

    decided on application model and design, working on AppSpace.js

--- client/src/App.js
+++ client/src/App.js
@@ -32,59 +32,6 @@ function App() {
   return (
     <div className="App">
 
-      <Grid className={mui.grid} container spacing={3}>
-        <Grid item xs={12}>
-          <Paper elevation={2} className={mui.paper}>
-            <Typography>
-              <div className="container-fluid jumbotron bg-dark border border-warning">
-                <Paper elevation={2}>
-                  <h1>Hello React and Hello World</h1>
-                  <Typography> Whats up </Typography>
-                </Paper>
-              </div>
-            </Typography>
-          </Paper>
-        </Grid>
-      </Grid>
-
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper elevation={1} className={mui.paper}>
-
-            <Grid container spacing={3}>
-              <Grid item xs={3}>
-                <Paper elevation={3} className={mui.paper}>
-                  <Typography>
-                    Hello React
-                  </Typography>
-                </Paper>
-              </Grid>
-            </Grid>
-
-            <Grid container spacing={3}>
-              <Grid item xs={3}>
-                <Paper elevation={3} className={mui.paper}>
-                  <Typography>
-                    Hello React
-                  </Typography>
-                </Paper>
-              </Grid>
-            </Grid>
-
-            <Grid container spacing={3}>
-              <Grid item xs={3}>
-                <Paper elevation={3} className={mui.paper}>
-                  <Typography>
-                    Hello React
-                  </Typography>
-                </Paper>
-              </Grid>
-            </Grid>
-
-
-          </Paper>
-        </Grid>
-      </Grid>
 
 
     </div>
---
+++ client/src/Components/App/AppSpace/AppSpace.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AppSpace.css';
+
+// -> Material-UI <-
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
