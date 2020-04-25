12399615b2a17b773fc8b4049049052811a8410c
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-04-24 22:53:45 -0400

    implemented use styles from @material-UI/core/styels

--- client/src/Components/App/AppSpace/AppSpace.js
+++ client/src/Components/App/AppSpace/AppSpace.js
@@ -8,3 +8,22 @@ import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    backgroundColor: "grey"
+  },
+  grid: {
+    backgroundColor: "black"
+  }
+
+
+}));
+
+
