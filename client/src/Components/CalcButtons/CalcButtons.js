2ac6eb2fa4ac895753321207b6905f5a86b99b72
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-04-25 20:45:18 -0400

    Began working on the application starting with the calcButtons

---
+++ client/src/Components/CalcButtons/CalcButtons.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './CalcButtons.css';
+
+// -> Material-UI <-
+import { makeStyles, MuiThemeProvider } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    backgroundColor: "grey"
+  },
+  grid: {
+    backgroundColor: "black"
+  }
+
+
+}));
+
+
+
+function Button() {
+
+  const mui = useStyles();
+
+  return (
+
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Paper elevation={3} className={mui.paper}>
+
+          <div className="AppBodyMain">
+            <div className="AppBody">
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <div className='Application'> Hello
+
+                  </div>
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+
+        </Paper>
+      </Grid>
+    </Grid>
+
+
+
+  );
+}
+
+export default CalcButton;
